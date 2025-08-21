a2b345b44a8dfd0691ededd90cca46f1ad46f676
Vinicius Dotto <dotto.vinicius7@gmail.com>
2025-08-21 19:37:35 -0300

    refactor: refatora header, altera scss de cores

--- js/index.js
+++ js/index.js
@@ -8,14 +8,23 @@ titleButton.addEventListener('click', function () {
     title.style.display = 'grid';
     experience.style.display = 'none';
     education.style.display = 'none';
+    titleButton.classList.add('active');
+    experienceButton.classList.remove('active');
+    educationButton.classList.remove('active');
 });
 experienceButton.addEventListener('click', function () {
     title.style.display = 'none';
     experience.style.display = 'grid';
     education.style.display = 'none';
+    titleButton.classList.remove('active');
+    experienceButton.classList.add('active');
+    educationButton.classList.remove('active');
 });
 educationButton.addEventListener('click', function () {
     title.style.display = 'none';
     experience.style.display = 'none';
     education.style.display = 'grid';
+    titleButton.classList.remove('active');
+    experienceButton.classList.remove('active');
+    educationButton.classList.add('active');
 });
--- ts/index.ts
+++ ts/index.ts
@@ -10,16 +10,25 @@ titleButton.addEventListener('click', (): void => {
     title.style.display = 'grid';
     experience.style.display = 'none';
     education.style.display = 'none';
+    titleButton.classList.add('active');
+    experienceButton.classList.remove('active'); 
+    educationButton.classList.remove('active');
 });
 
 experienceButton.addEventListener('click', (): void => {
     title.style.display = 'none';
     experience.style.display = 'grid';
     education.style.display = 'none';
+    titleButton.classList.remove('active');
+    experienceButton.classList.add('active'); 
+    educationButton.classList.remove('active');
 });
 
 educationButton.addEventListener('click', (): void => {
     title.style.display = 'none';
     experience.style.display = 'none';
     education.style.display = 'grid';
+    titleButton.classList.remove('active');
+    experienceButton.classList.remove('active'); 
+    educationButton.classList.add('active');
 });
